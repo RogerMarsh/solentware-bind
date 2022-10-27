517ad10be121a0439c66d54157bf373acf297d4e
Roger Marsh <roger.marsh@solentware.co.uk>
2022-10-27 11:23:09 +0000

    Move gridbindings module from solentware_bind to solentware_grid.

--- solentware_bind/gui/__init__.py
+++ solentware_bind/gui/__init__.py
@@ -4,13 +4,9 @@
 
 """Define bind and try methods for tkinter callbacks.
 
-The gridbindings module provides standard bindings used by applications
-available on www.solentware.co.uk.  The gridbindings.GridBindings class
-expects to be a superclass alongside the solentware_grid.datagrid.DataGrid
-class, but the setup module for solentware_misc does not declare the
-dependency.
-It is assumed the solentware_grid package will be present if gridbindings is
-used.
+The bindings module provides wrappers for tkinter bind and bind_tag methods
+to allow bindings to be cancelled without memory leak, as described in
+tkinter.Misc.bind doctring.
 
 The exceptionhandler module provides a widget for displaying an exception
 report for an exception which is causing the application to stop.
